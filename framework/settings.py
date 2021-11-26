cb7a75deaa51fa5a2061287a58f8257fedaf1b24
Chainksain <chainksain@hotmail.com>
2021-11-26 11:32:10 -0300

    heroku deploy update

--- framework/settings.py
+++ framework/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'framework.urls'
@@ -128,11 +128,11 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-#STATICFILES_DIRS = [
-#    BASE_DIR / 'static',
-#]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
