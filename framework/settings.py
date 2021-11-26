c0201f9b1737622b6d7b8e101a0e5be5b452cb3d
Chainksain <chainksain@hotmail.com>
2021-11-26 10:48:46 -0300

    heroku deploy update

--- framework/settings.py
+++ framework/settings.py
@@ -125,14 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    'https://retifica.herokuapp.com/static',
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static',
+#    'https://retifica.herokuapp.com/static',
+#]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
