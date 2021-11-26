9e8623e3d36dacb79129d89f7b3fc9d1606a39c3
Chainksain <chainksain@hotmail.com>
2021-11-26 10:36:34 -0300

    heroku deploy update

--- framework/settings.py
+++ framework/settings.py
@@ -129,9 +129,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static',
+#]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
